587b8d355747023a776bf4762d8871b0fd533e8d
lakelexy <alao8380@yahoo.com>
2024-12-18 12:18:25 +0100

    Setting up navigation between pages

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import Header from "@/components/layout/Header";
 
 const Posts: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">Posts</h1>
-      <p>List of posts will appear here.</p>
+    <div>
+      <Header />
+      <main className="container mx-auto py-8 px-4">
+	<h1 className="text-3xl font-bold mb-4">Posts</h1>
+	<p className="text-lg">List of posts will appear here.</p>
+      </main>
     </div>
   );
 };
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import Header from "@/components/layout/Header";
 
 const Users: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">Users</h1>
-      <p>List of users will appear here.</p>
+    <div>
+      <Header />
+      <main className="container mx-auto py-8 px-4">
+        <h1 className="text-3xl font-bold mb-4">Users</h1>
+        <p className="text-lg">List of users will appear here.</p>
+      </main>
     </div>
   );
 };
