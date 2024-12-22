15330d1e5670445be1c8b6871da19eab0ca92586
lakelexy <alao8380@yahoo.com>
2024-12-22 20:23:50 +0100

    including onclose and onsubmit in my interface

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,8 +1,18 @@
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
 export interface PostProps {
   userId: number;
   id: number;
   title: string;
   body: string;
+}
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
 }
 export interface UserProps {
   id: number;
