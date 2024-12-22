67c7ba99644b47c931f3dd79def5ffc069886d52
lakelexy <alao8380@yahoo.com>
2024-12-22 18:57:54 +0100

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl">Posts Page</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow p-6">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+        {/* Content for Posts */}
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl">Users Page</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow p-6">
+        <h1 className="text-3xl font-bold">Users Page</h1>
+        {/* Content for Users */}
+      </main>
     </div>
   );
 };
